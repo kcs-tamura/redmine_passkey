318768ffb7af3a0c84e259ac28f6efa50885e081
kuro <kuro@legion-neko.com>
2026-04-24 03:05:06 +0900

    Release: v1.1.0

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_passkey do
   name        'Redmine Passkey'
   author      'Kitsune Creative Studio'
   description 'WebAuthn/Passkey authentication for Redmine'
-  version     '1.0.1'
+  version     '1.1.0'
   author_url 'https://www.kitsune-creative.studio/'
   url         'https://github.com/kcs-tamura/redmine_passkey'
   requires_redmine version_or_higher: '4.2.0'
