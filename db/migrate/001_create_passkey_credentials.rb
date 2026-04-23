3b8eadb2eeaf3fc138374efff90ba988431f4e4c
kuro <kuro@legion-neko.com>
2026-04-23 23:26:51 +0900

    fix: migrate files.

--- db/migrate/001_create_passkey_credentials.rb
+++ db/migrate/001_create_passkey_credentials.rb
@@ -1,13 +1,16 @@
 class CreatePasskeyCredentials < ActiveRecord::Migration[6.1]
   def change
     create_table :passkey_credentials do |t|
-      t.references :user,        null: false, foreign_key: true
+      t.integer    :user_id,     null: false
+
       t.string     :external_id, null: false
       t.string     :nickname,    null: false, default: 'My Passkey'
       t.string     :public_key,  null: false
       t.integer    :sign_count,  null: false, default: 0
       t.timestamps
     end
+    add_index :passkey_credentials, :user_id
     add_index :passkey_credentials, :external_id, unique: true
+    add_foreign_key :passkey_credentials, :users
   end
 end
